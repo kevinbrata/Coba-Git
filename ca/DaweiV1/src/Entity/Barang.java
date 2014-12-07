017d549d65a4dcbbba45ad5658e92981ab21ed09
Kevin Brata <kevin.brata@hotmail.com>
2014-12-07 15:41:33 +0700

    bara bara bara

---
+++ ca/DaweiV1/src/Entity/Barang.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Cavin Wijaya
+ */
+@Entity
+@Table(name = "barang")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Barang.findAll", query = "SELECT b FROM Barang b"),
+    @NamedQuery(name = "Barang.findByKodeBarang", query = "SELECT b FROM Barang b WHERE b.kodeBarang = :kodeBarang"),
+    @NamedQuery(name = "Barang.findByNamaBarang", query = "SELECT b FROM Barang b WHERE b.namaBarang = :namaBarang"),
+    @NamedQuery(name = "Barang.findByHarga", query = "SELECT b FROM Barang b WHERE b.harga = :harga"),
+    @NamedQuery(name = "Barang.findByStok", query = "SELECT b FROM Barang b WHERE b.stok = :stok")})
+public class Barang implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "kode_barang")
+    private String kodeBarang;
+    @Basic(optional = false)
+    @Column(name = "nama_barang")
+    private String namaBarang;
+    @Basic(optional = false)
+    @Column(name = "harga")
+    private int harga;
+    @Basic(optional = false)
+    @Column(name = "stok")
+    private int stok;
+
+    public Barang() {
+    }
+
+    public Barang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
+    }
+
+    public Barang(String kodeBarang, String namaBarang, int harga, int stok) {
+        this.kodeBarang = kodeBarang;
+        this.namaBarang = namaBarang;
+        this.harga = harga;
+        this.stok = stok;
+    }
+
+    public String getKodeBarang() {
+        return kodeBarang;
+    }
+
+    public void setKodeBarang(String kodeBarang) {
+        this.kodeBarang = kodeBarang;
+    }
+
+    public String getNamaBarang() {
+        return namaBarang;
+    }
+
+    public void setNamaBarang(String namaBarang) {
+        this.namaBarang = namaBarang;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getStok() {
+        return stok;
+    }
+
+    public void setStok(int stok) {
+        this.stok = stok;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (kodeBarang != null ? kodeBarang.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Barang)) {
+            return false;
+        }
+        Barang other = (Barang) object;
+        if ((this.kodeBarang == null && other.kodeBarang != null) || (this.kodeBarang != null && !this.kodeBarang.equals(other.kodeBarang))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity.Barang[ kodeBarang=" + kodeBarang + " ]";
+    }
+    
+}
